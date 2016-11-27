8093c7b5eb1fc13c4ae1f9a1a10107bc46ac128c
Trevor Carr <trevor@trevorsee.com>
2016-11-26 22:54:50 -0500

    slideshow and details

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+    });
+});
